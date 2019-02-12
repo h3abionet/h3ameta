72e2735592bd092f68c25152152c55a67b14fedc
Mushal Ali (H3AMeta) <mushalali@n39.core.wits.ac.za>
2019-02-12 17:40:09 -0500

    straincomp_13-02-2019

---
+++ straincomp/work/2e/6ff4da643e885b975b33ca2d6f2a49/.command.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+srst2 --input_se test4.fq --output test4_srst2 --gene_db ResFinder.fasta
---
+++ straincomp/work/4e/f5fa03e0e3355aa94902199bd059b7/.command.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+bracken -d /local/kraken/mini8GB -i test2_kraken.tsv 	-o test2_kraken_bracken.tsv -r 100 -l S
