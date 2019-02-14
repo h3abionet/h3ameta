10c4242305dee8fb30c8424c854e865acd9f6bb8
Mushal Ali (H3AMeta) <mushalali@cream-ce.core.wits.ac.za>
2019-02-14 17:52:52 +0200

    mushal_14Feb2019

---
+++ straincomp/work/83/8e664a7a69285b89e5e20968f34d2a/.command.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+srst2 --input_se test2.fq --output test2_srst2 --gene_db ARGannot_r3.fasta
---
+++ straincomp/work/f7/9a96e80a5f36e6396a6a97e004a349/.command.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+srst2 --input_se test3.fq --output test3_srst2 --gene_db ARGannot_r3.fasta
